2a7a80568ba49031af1b2cf958b49aee01b9e1a4
Ayolawra <ayolawra@gmail.com>
2023-02-16 18:49:57 -0800

    print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
-# include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Description: use printf function
+ * Return: 0
+ */
+
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return(0);
 }
