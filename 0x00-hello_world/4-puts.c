6bf1ffc00935ed3ad1421b0f31b03a7b32dd805f
Ayolawra <ayolawra@gmail.com>
2023-02-16 18:33:52 -0800

    checking

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main-entry point
- * Description: Prints "\"Programming is like building a multilingual puzzle"
- * Returns 0
- */
+* main - Entry point
+* Description: Prints "\"Programming is like building a multilingual puzzle"
+* Returns: 0
+*/
 
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-
-}
-	
+}	
