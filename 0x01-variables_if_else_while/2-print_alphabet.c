4a70c48b0487f83e0ad30c425ae7f9374bada733
Ayolawra <ayolawra@gmail.com>
2023-02-17 17:15:06 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -Enter main function
+ * Description: print alphabet
+ *
+ * Return: char
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
