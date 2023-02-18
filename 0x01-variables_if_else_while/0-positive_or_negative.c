d55a40c35bc1ebdeaa2b31be5d3eeb1a47f39b58
Ayolawra <ayolawra@gmail.com>
2023-02-17 16:16:52 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * Main - returns random integer
+/**
+ * main -returns random integer
  * Description: This function returns a random nuber for certain conditions
+ *
  * Return: 0
  */
 int main(void)
@@ -14,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive ", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative ", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is zero ", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
