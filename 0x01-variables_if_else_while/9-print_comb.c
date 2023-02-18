1913b105bb2bdf18729b73464fe2940527cad94d
Ayolawra <ayolawra@gmail.com>
2023-02-17 18:33:29 -0800

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -Enter main function
+ * Description: program that prints combinations of single digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	{
+		putchar((char) (num + 48));
+
+		if (num == 0)
+
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+		if (num == 9)
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
