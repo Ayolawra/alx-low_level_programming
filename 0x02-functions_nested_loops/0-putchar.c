62e2e8d71b8c435f1a1dd884a3b57b19b0058576
Ayolawra <ayolawra.gmail.com>
2023-02-21 14:04:45 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main -returns the function
+ * Description: Program prints _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory explores nested loops
