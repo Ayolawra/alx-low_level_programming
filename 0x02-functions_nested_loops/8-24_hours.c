098d84dcc6a3f6308f73c8eebe51f77cc43d346e
Ayolawra <ayolawra@gmail.com>
2023-02-22 11:21:12 -0800

    print minute of day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - Function that prints every minute of jack's day
+ *
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 3; a++)
+
+	{
+		if (a == 2)
+		{
+			for (b = 0; b < 4; b++)
+			{
+				for (c = 0; c < 6; c++)
+				{
+					for (d = 0; d < 10; d++)
+					{
+						printf("%d%d:%d%d\n", a, b, c, d);
+					}
+				}
+			}
+		}
+		else
+		{
+			for (a = 0; a < 9; a++)
+			for (b = 0; b < 10; b++)
+			{
+				for (c = 0; c < 6; c++)
+				{
+					for (d = 0; d < 10; d++)
+					{
+						printf("%d%d:%d%d\n", a, b, c, d);
+					}
+				}
+			}
+		}
+	}
+}
