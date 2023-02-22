f170eabf82658c3fd73a5d1fc94d7ee53750080c
Maromitan Laura <54981873+Ayolawra@users.noreply.github.com>
2023-02-22 21:15:36 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,4 +7,32 @@
  * @n: variable to check
  * Return: int
  */
+void print_to_98(int n)
+{
+  if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			};
+			n++;
+			}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+		{
+			printf(", ");
+		}
+		n--;
+		}
+	}
 
+	printf("\n");
+}
