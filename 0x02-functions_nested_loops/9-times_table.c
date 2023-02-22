fd5522725a7b3315639f6d0ea565eb588c3a1e6d
Ayolawra <ayolawra@gmail.com>
2023-02-22 11:55:27 -0800

    9 times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - print 9 times table
+ *
+ * Return: int 0
+ */
+void times_table(void)
+{
+	int i;
+	for (i = 0; i < 13; i++)
+		return (i * 9);
+}
