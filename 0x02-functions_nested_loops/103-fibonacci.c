d40a04674b46a40d5c8bfb44b0051e9e5911afa6
Maromitan Laura <54981873+Ayolawra@users.noreply.github.com>
2023-02-22 21:49:04 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
+/**
+ * main - function print even numbers of the first 50 fibonacci
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	long int a, b = 1, c = 2, sum = 0, eSum = 0;
 
+	for (a = 0; a < 49; a++)
+
+	{
+		if ((c % 2 == 0) && (c <= 4000000))
+		{
+			eSum = eSum + c;
+		}
+	sum = b + c;
+b = c;
+c = sum;
+	}
+	printf("%ld\n", eSum);
+	return (0);
+}
