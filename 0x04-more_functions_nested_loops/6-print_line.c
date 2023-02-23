d1517260fad82bae4817f4bb05af173617e3144c
Ayolawra <ayolawra@gmail.com>
2023-02-23 14:36:25 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - prints a line n chars long
+ * @n: length of line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			putchar('_');
+		}
+	putchar('\n');
+	}
+}
