fbd99670a52d8d3768ed7216df98c61b7f452bcd
Ayolawra <ayolawra@gmail.com>
2023-02-23 14:50:08 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints square of size size
+ * @size: size of square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			putchar('#');
+			for (j = 1; j < size; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
