5752b9277559fa565b718b41a4c32828dc6b6713
Ayolawra <ayolawra@gmail.com>
2023-02-23 14:09:47 -0800

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,16 +12,6 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
-		if ((i == '2') || (i == '4'))
-		{
-			continue;
-		}
-		_putchar('0' + i);
-	}
-
-	_putchar('\n');
-
-
-
+		(i != 2 && i != 4) ? putchar(i + '0') : 0; /* ternary operator */
+	putchar('\n');
 }
