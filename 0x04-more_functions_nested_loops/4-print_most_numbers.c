5a2a1323e1b1695a7669ce85ecc5270b80e7e778
Ayolawra <ayolawra@gmail.com>
2023-02-23 13:52:39 -0800

    check for number except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function to print numbers
+ * except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+		if ((i == '2') || (i == '4'))
+		{
+			continue;
+		}
+		putchar('0' + i);
+
+	putchar('\n');
+
+
+
+}
