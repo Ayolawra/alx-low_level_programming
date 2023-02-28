9ff56db6f4a731c3d3773900a0d0b4e3cdf6ddfe
Ayolawra <ayolawra@gmail.com>
2023-02-27 16:01:35 -0800

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string to act on
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (str[i])
+	{
+		putchar(str[i__]);
+	}
+	putchar('\n');
+}
