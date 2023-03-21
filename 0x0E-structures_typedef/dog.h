4b1be5f4b1a6f0d1ad3f1f41c3a749577c32f62f
Ayolawra <ayolawra@gmail.com>
2023-03-21 12:47:13 -0700

    type dog with elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a type with elements
+ * @name: the name element
+ * @age: the age element
+ * @owner: the owner element
+ *
+ * Description: a structure of type dog with elements
+ */
+
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+
+#endif
